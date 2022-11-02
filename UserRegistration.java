10676225656fca51f82bf753426bf64333b1d121
sayali-2001 <sayalithakare47@gmail.com>
2022-11-02 22:35:35 +0530

    Enter a Valid First Name

---
+++ UserRegistration.java
@@ -0,0 +1,35 @@
+package Day23;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    public static boolean isValidUsername(String name)
+    {
+
+        String regex = "^[A-Za-z]\\w{5,29}$";
+
+        Pattern p = Pattern.compile(regex);
+
+
+        if (name == null) {
+            return false;
+        }
+
+        Matcher m = p.matcher(name);
+
+
+        return m.matches();
+    }
+
+    // Driver Code
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Name");
+        String fname = sc.nextLine();
+        System.out.println(isValidUsername(fname));
+    }
+
+
+}
+
