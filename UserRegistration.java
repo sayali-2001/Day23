8b67de65d74995a9195eb076317fe39cb970db02
sayali-2001 <sayalithakare47@gmail.com>
2022-11-03 21:20:53 +0530

    Added: Enter Valid PassWord: Rule 1

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,12 +2,14 @@ package Day23;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+//Enter Valid Password
+//Should Have Minimum 8 Characters
 public class UserRegistration {
     public static boolean isValidUsername(String name)
     {
 
-        String regex = "^[A-Za-z]\\w{5,29}$";
+//        String regex = "[789]{1}[0-9]{9}";
+        String regex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]$";
 
         Pattern p = Pattern.compile(regex);
 
@@ -25,9 +27,9 @@ public class UserRegistration {
     // Driver Code
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Your Name");
-        String fname = sc.nextLine();
-        System.out.println(isValidUsername(fname));
+        System.out.println("Enter Your Correct Password");
+        String pass = sc.nextLine();
+        System.out.println(isValidUsername(pass));
     }
 
 
