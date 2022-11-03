534b216f0189ab2cd61e9ed3318e20832a593362
sayali-2001 <sayalithakare47@gmail.com>
2022-11-03 23:02:06 +0530

    Added: Clear all Email Samples

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,15 +2,15 @@ package Day23;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-//Enter Valid Password
-//Should Have Exactly 1 Special Character
+//Enter Valid Emails
+
 public class UserRegistration {
     public static boolean isValidUsername(String name)
     {
 
 //        String regex = "[789]{1}[0-9]{9}";
-        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).(?=.*[-+_!@#$%^&*.,?])+$";
-
+//        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).(?=.*[-+_!@#$%^&*.,?])+$";
+String regex = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$";
 
         Pattern p = Pattern.compile(regex);
 
@@ -28,7 +28,7 @@ public class UserRegistration {
     // Driver Code
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Your Correct Password");
+        System.out.println("Enter Your Emails");
         String pass = sc.nextLine();
         System.out.println(isValidUsername(pass));
     }
